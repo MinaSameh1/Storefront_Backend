11730a5c4fd407d8231925035f860a5c0d73cb23
MinaSameh1 <minasameh1@gmail.com>
2022-10-02 20:05:22 +0200

    Added graceful shutdown, and db connect.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,45 @@
 import { createExpressApp } from './server'
-import { logger } from './utils'
+import { logger, connect, disconnect } from './utils'
 
 /*
  * @description Starts the expressjs server
  */
 function startServer() {
   const Port = process.env.PORT || 8000
-  const app = createExpressApp()
-  app.listen(Port, () => {
+  const app = createExpressApp() // Get our express instance
+  connect() // Database
+  return app.listen(Port, () => {
     logger.info(`API is listening to ${Port}`)
   })
 }
 
-startServer()
+/*
+ * @description Gracefully shutdowns the server.
+ * - Disconnects from db
+ */
+function shutdown(signal: string) {
+  return async (err: unknown) => {
+    logger.info(`Recieved ${signal}, shutting down gracefully`)
+    if (err) {
+      logger.error(err)
+    }
+
+    setTimeout(function () {
+      logger.error('could not close gracefully, bailling.')
+      process.exit(1)
+    }, 10000)
+
+    server.close(async () => {
+      disconnect()
+      logger.info('Server Shutdown gracefully')
+      process.exit(err ? 1 : 0)
+    })
+  }
+}
+
+const server = startServer()
+
+process
+  .on('SIGTERM', shutdown('SIGTERM'))
+  .on('SIGINT', shutdown('SIGINT'))
+  .on('uncaughtException', shutdown('uncaughtException'))

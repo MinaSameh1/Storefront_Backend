d0a2993e5ab46f5c501e360b32d2b954d0b5a756
MinaSameh1 <minasameh1@gmail.com>
2022-10-12 03:20:27 +0200

    refactor(validate middleware): added comments and JSDoc

--- src/middleware/validateBody.middleware.ts
+++ src/middleware/validateBody.middleware.ts
@@ -1,14 +1,21 @@
 import { NextFunction, Request, Response } from 'express'
 import { validateType } from '../types'
 
-// Created this to make sure the body is good
+/**
+ * @description Validates Body to be according to template
+ * @param {validateType} Body : Template of the body, see ValidateType.
+ * Continues to next handler or returns 400 with message.
+ */
 export const validateBody =
   (body: validateType) => (req: Request, res: Response, next: NextFunction) => {
     try {
-      // loop through the req.body
+      // loop through the template keys
       for (const key of Object.keys(body)) {
+        // If the value doesn't exist on body
         if (req.body[key] === undefined) {
+          // Check if its optional or not
           if (!body[key].optional) throw Error(`Missing ${key}!`)
+          // Check if the value is the same type as the template.
         } else if (typeof req.body[key] !== body[key].type)
           throw Error(
             `Wrong type of ${key}, should be ${
@@ -19,6 +26,7 @@ export const validateBody =
       next()
     } catch (err) {
       if (err instanceof Error)
+        // Returns 400 (Bad Request) with msg on why it happened.
         return res.status(400).json({ message: err.message, error: true })
     }
   }

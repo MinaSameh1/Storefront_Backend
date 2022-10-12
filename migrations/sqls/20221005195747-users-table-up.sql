b24b9623a9e3cde0b34f870fbd669d1104ec454a
MinaSameh1 <minasameh1@gmail.com>
2022-10-12 04:12:04 +0200

    Added username.

--- migrations/sqls/20221005195747-users-table-up.sql
+++ migrations/sqls/20221005195747-users-table-up.sql
@@ -2,6 +2,7 @@
 -- which is close to default `user` table of postgres.
 CREATE TABLE IF NOT EXISTS store_users(
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
     firstname TEXT NOT NULL,
     lastname TEXT NOT NULL,
     pass TEXT NOT NULL
@@ -9,18 +10,19 @@ CREATE TABLE IF NOT EXISTS store_users(
 
 --- Inserts users.
 CREATE OR REPLACE FUNCTION
-insert_user(_firstName TEXT, _lastName TEXT, _pass TEXT)
+insert_user(_username TEXT, _firstName TEXT, _lastName TEXT, _pass TEXT)
 RETURNS TABLE(
     id UUID,
+    username TEXT,
     firstname TEXT,
     lastname TEXT
 ) AS
 $$
 BEGIN
   RETURN QUERY
-    INSERT INTO store_users(firstName, lastName, pass)
-    VALUES(_firstName, _lastName, _pass)
-    RETURNING store_users.id, store_users.firstname, store_users.lastname;
+    INSERT INTO store_users(username, firstName, lastName, pass)
+    VALUES(_username, _firstName, _lastName, _pass)
+    RETURNING store_users.id, store_users.username, store_users.firstname, store_users.lastname;
 END
 $$
 LANGUAGE 'plpgsql';

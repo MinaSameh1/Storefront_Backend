f72c0d915f1a90cd8d216503254c0be33780e3f1
MinaSameh1 <minasameh1@gmail.com>
2022-10-19 05:22:09 +0200

    Added data

---
+++ data.sql
@@ -0,0 +1,131 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.5 (Debian 14.5-1.pgdg110+1)
+-- Dumped by pg_dump version 14.5 (Debian 14.5-1.pgdg110+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: store_users; Type: TABLE DATA; Schema: public; Owner: root
+--
+
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('8120bea4-e571-45d0-849d-b5036d81c646', 'Delfina.Yundt46', 'Delfina', 'Yundt', '3v7iPExehQWX5Fd');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('ed5d150c-16c6-45e1-bc36-dcaf28701efb', 'Ellis.Strosin43', 'Ellis', 'Strosin', 'QJ4uzMagsWGiyfQ');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('17808ef7-2ef2-4af6-92f1-9d206feba93f', 'Maynard_Rempel', 'Maynard', 'Rempel', '$2b$10$V6Eg1PobfVpXcgOK3qakqugkTV9ydcSv6HXSH3YEZ7lQfXBsESkTa');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('3045e9f9-b23e-4b35-b3bf-193f468fd2d1', 'Okey7', 'Okey', 'Crooks', '$2b$10$I1oIlP9HvttwQIfmoSMHLOAGLp5K10EiD5dLERZ39.UrOCByXSOcK');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('971e2a47-0409-43e7-b3e3-56c5fefa046a', 'Alva.Schimmel68', 'Alva', 'Schimmel', '$2b$10$7gBEdhvDo..taj4tm5UlCe8FRTrrkLP1gJWeDydyMFzKP1B6Dq0Ta');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('b60d751c-5feb-4a7b-9ae2-2d61befb4b44', 'Enoch.Kulas35', 'Enoch', 'Kulas', '$2b$10$g5zh2Ak96qy.N7ShyffMYOryrJ2X9AkBaSp2dqdJJnpG9zD/6euE.');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('d393ea55-24ca-419e-bece-7c7366151747', 'Ebony.Douglas89', 'Ebony', 'Douglas', '$2b$10$YB1gI9EnJjd5.6H51E.PZei11bPZbnMKI0LLCTVhmE9Jofc8kvRbm');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('3b446d75-a0b4-448e-8b04-0d55ae364b92', 'Telly_Miller', 'Telly', 'Miller', '$2b$10$wkLRLn9olMJnxmmq1JG2vuyb.xL/D6JDa5UMnExXWjDxi1zRRsj4G');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('d969da2d-a31f-4b5e-9a41-b5242a75bdc3', 'Lucius91', 'Lucius', 'Metz', '$2b$10$6mQXKDO0AODF1hrwi5E0Eu0.wqQe4eLv/PLu.rL5614R5MJFJaDGG');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('4e007d3a-5358-4f0d-84a7-4e060ee62f18', 'Ladarius_Dooley22', 'Ladarius', 'Dooley', '$2b$10$0SEiGu/3vWwXclZ9WwC85echh6PzzlM4MYR/gbx5Uj2tKgAWF.nYK');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('777bdb86-9cda-451b-8c0d-5369c77a3d05', 'Cathy.Kozey', 'Cathy', 'Kozey', 'Br3s7yvmR6MkpdN');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('5eedab37-87e7-4c43-bdb1-590dae037406', 'Dayne33', 'Dayne', 'Gislason', '_7Lx9hetUmfivxj');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('82526f39-4769-474d-86f8-16608ca1f94c', 'Houston48', 'Houston', 'Bogan', 'SSqArC7JAbpGF9k');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('cb16f21b-e77b-4c00-b77b-505ec3faa8a6', 'Gia4', 'Gia', 'Crona', 'kF1zEAc0RiL8QJV');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('61b0d632-dbbb-403c-a880-1ec80e580211', 'Angelica.Volkman', 'Angelica', 'Volkman', 'fB3Wfy_yD90axmO');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('1afaebd7-84e6-4e85-ae17-924db1811541', 'Friedrich_Rogahn70', 'Friedrich', 'Rogahn', 'es5eyjZ44cEiqYV');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('712a1941-743f-48bc-b00c-8ac52a24d235', 'Abdul22', 'Abdul', 'Kutch', 'v4sPro049no1SRy');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('746c0cef-1938-4e31-8d9e-9be74679886d', 'Julian.Johnston', 'Julian', 'Johnston', '0ghMNkfUEGe0XI6');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('92650296-e149-49df-866e-00f07d8955ca', 'Jacinthe_Reinger', 'Jacinthe', 'Reinger', 'H3jJx1TuGBj16to');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('d77efb91-b71b-462f-9ede-aa99243379f9', 'Sylvester_Keebler', 'Sylvester', 'Keebler', 'Is4Tz_hBltUsdEn');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('62c4d07e-7c11-41c8-9534-cd51300330fe', 'Camryn_Metz', 'Camryn', 'Metz', 'HyB_RJM0T5jFiRH');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('6d05b12a-7830-474e-acc4-7ffad6b4a4bc', 'Broderick.Nitzsche', 'Broderick', 'Nitzsche', 'j8zOt8LmOoqZ5Xe');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('65edb567-463c-4187-a241-56845a33dd43', 'Hosea53', 'Hosea', 'Oberbrunner', 'waA5ggc8Nt6F7cX');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('486dd667-6a13-4857-ab57-d3eeb6bbada5', 'Vernie.Kunde86', 'Vernie', 'Kunde', 'rQ9urAgrisPH0rg');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('7a3b4a51-1ecb-489c-af82-5640544df3f3', 'Toney.Breitenberg', 'Toney', 'Breitenberg', '$2b$10$4jbFbz4CXOeN97hX.uvxc.wi4KRNVi8EL2GoHSHHAsZl./ZJwxtH6');
+INSERT INTO public.store_users (id, username, firstname, lastname, pass) VALUES ('f6266e6d-1154-4556-897c-42aac57a266f', 'Golda.Toy55', 'Golda', 'Toy', '$2b$10$zxr5fmA/bTU39Gx0JAqBK.g0THVmhqWwB4wgVE7jgCO257Ph07fgu');
+
+
+--
+-- Data for Name: orders; Type: TABLE DATA; Schema: public; Owner: root
+--
+
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('dfc74bad-20ad-495b-812a-ed35b97afdd8', '8120bea4-e571-45d0-849d-b5036d81c646', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('0617a1b7-0439-4876-873d-6e075a57e600', '8120bea4-e571-45d0-849d-b5036d81c646', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('00243f9a-a636-4ba5-91de-be95a91c7ba7', '8120bea4-e571-45d0-849d-b5036d81c646', false);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('b78f8c4b-4512-4f23-a741-2d77f524fedc', 'ed5d150c-16c6-45e1-bc36-dcaf28701efb', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('dfc54a61-9663-42e1-bb5d-28e88a019325', 'ed5d150c-16c6-45e1-bc36-dcaf28701efb', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('fe339b8a-ba0b-4137-8ba1-8fc7943d3b52', 'ed5d150c-16c6-45e1-bc36-dcaf28701efb', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('c5c267d2-c582-47aa-aff6-207d10db63ac', '777bdb86-9cda-451b-8c0d-5369c77a3d05', false);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('534f9e45-7c94-4150-b633-c1a8de3ce466', '5eedab37-87e7-4c43-bdb1-590dae037406', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('393201dd-d950-4b48-a5b4-d9f60175ba5b', '5eedab37-87e7-4c43-bdb1-590dae037406', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('1a2c31d4-0212-4e5f-a81d-aee0a17e68ba', '5eedab37-87e7-4c43-bdb1-590dae037406', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('9652db9e-c213-460a-b864-1e3ce34ffa98', '5eedab37-87e7-4c43-bdb1-590dae037406', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('03f5577c-ede4-482a-8df0-42e2ec6c770d', '5eedab37-87e7-4c43-bdb1-590dae037406', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('764fb64b-136f-41e7-8f57-94a7b1691b6e', '5eedab37-87e7-4c43-bdb1-590dae037406', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('823846ec-9bba-41f4-9958-d80ea35bbfcf', '5eedab37-87e7-4c43-bdb1-590dae037406', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('d0520657-bb5c-42d6-a6ef-e5c93ba96dd4', '5eedab37-87e7-4c43-bdb1-590dae037406', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('892ef20e-6237-400a-8e5e-696e970d8198', '92650296-e149-49df-866e-00f07d8955ca', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('7fd31fb0-83ed-4ae8-9c58-95b8e606c14f', '92650296-e149-49df-866e-00f07d8955ca', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('2accbbe2-bd19-4091-9456-0fc0b0407582', '92650296-e149-49df-866e-00f07d8955ca', true);
+INSERT INTO public.orders (id, user_id, order_status) VALUES ('ee1efb64-ae33-4963-bd3f-e6263dcf41a6', 'd77efb91-b71b-462f-9ede-aa99243379f9', false);
+
+
+--
+-- Data for Name: products; Type: TABLE DATA; Schema: public; Owner: root
+--
+
+INSERT INTO public.products (id, name, price, category) VALUES ('10a6fcf5-7cc3-4b92-b700-0c8c92f5a51c', 'Handmade Plastic Mouse', '$379.00', 'Metal');
+INSERT INTO public.products (id, name, price, category) VALUES ('88b62eb7-46f9-4929-ae95-9d7f06b7c009', 'Practical Rubber Cheese', '$644.00', 'test');
+INSERT INTO public.products (id, name, price, category) VALUES ('9755a6c3-6a82-4295-974b-b2354b46ef17', 'Refined Concrete Cheese', '$579.00', NULL);
+INSERT INTO public.products (id, name, price, category) VALUES ('a1ab7d32-75bf-4c28-90c7-4c4167e1e6bd', 'Awesome Wooden Shirt', '$602.00', 'Wooden');
+INSERT INTO public.products (id, name, price, category) VALUES ('81294c42-7686-4ddd-a324-5a8c3893d16c', 'Generic Wooden Shoes', '$841.00', 'Plastic');
+INSERT INTO public.products (id, name, price, category) VALUES ('25ddf40b-6dd9-4da2-a289-26271cf95f0a', 'Modern Metal Tuna', '$243.00', 'testModel');
+INSERT INTO public.products (id, name, price, category) VALUES ('85a0e1ff-4b4b-4f14-8edb-80503e2218c7', 'Luxurious Metal Computer', '$32.00', 'testModel');
+INSERT INTO public.products (id, name, price, category) VALUES ('50c5a19d-203e-4a69-a683-dc644bc4e8ab', 'Tasty Soft Chicken', '$513.00', 'testModel');
+INSERT INTO public.products (id, name, price, category) VALUES ('fb04b5d3-bb83-4be5-b4d0-59885b604e7b', 'Luxurious Frozen Tuna', '$221.00', 'testModel');
+INSERT INTO public.products (id, name, price, category) VALUES ('e4f4bc28-9e3c-4387-9173-23bb6cf4a167', 'Intelligent Steel Chair', '$8.00', 'testModel');
+INSERT INTO public.products (id, name, price, category) VALUES ('efe674ee-d244-4e05-b597-f893fc02795a', 'Small Wooden Salad', '$301.00', 'Soft');
+INSERT INTO public.products (id, name, price, category) VALUES ('48165781-3af6-4e43-ac7f-3e5828630159', 'Intelligent Cotton Fish', '$337.00', 'testModel');
+INSERT INTO public.products (id, name, price, category) VALUES ('78e18be2-d004-42e6-9f6c-870ed09f260e', 'Handcrafted Frozen Shoes', '$834.00', 'Bronze');
+INSERT INTO public.products (id, name, price, category) VALUES ('96ccc85f-b733-4435-95ab-91bd9644e5c7', 'Electronic Granite Computer', '$257.00', 'Rubber');
+INSERT INTO public.products (id, name, price, category) VALUES ('bdecbc3e-e120-44b3-856f-446e56d9b9c1', 'Small Steel Table', '$280.00', 'Fresh');
+INSERT INTO public.products (id, name, price, category) VALUES ('45701373-c9b7-46b6-9478-acb1c32d92e4', 'Refined Frozen Bacon', '$842.00', 'Soft');
+INSERT INTO public.products (id, name, price, category) VALUES ('ce9f3f64-92e5-4f14-8e26-c468d5516dff', 'Awesome Rubber Soap', '$222.00', 'Soft');
+INSERT INTO public.products (id, name, price, category) VALUES ('2b00bc97-a879-4e97-99ff-ac208bb187ae', 'Refined Steel Shoes', '$132.00', 'Bronze');
+INSERT INTO public.products (id, name, price, category) VALUES ('3bb5e79b-1509-471a-a848-ef4d308a35a8', 'Handcrafted Fresh Computer', '$210.00', 'testService');
+INSERT INTO public.products (id, name, price, category) VALUES ('73bec69f-aa5d-4bac-af61-697e89da01fb', 'Refined Soft Shoes', '$774.00', 'testService');
+INSERT INTO public.products (id, name, price, category) VALUES ('ae815ddd-27e9-4800-bc47-1b5531e9de9e', 'Licensed Concrete Keyboard', '$430.00', 'testService');
+INSERT INTO public.products (id, name, price, category) VALUES ('4807d8f3-4b7b-4707-a7ce-d435a29c4abf', 'Electronic Rubber Gloves', '$529.00', 'testService');
+INSERT INTO public.products (id, name, price, category) VALUES ('9887a3a2-c03b-4e3b-b4e8-f72640757f62', 'Intelligent Concrete Chips', '$4.00', 'testService');
+INSERT INTO public.products (id, name, price, category) VALUES ('66d289db-d78c-4ae8-974e-2b08676cc20b', 'Sleek Soft Chicken', '$140.00', 'testService');
+
+
+--
+-- Data for Name: order_item; Type: TABLE DATA; Schema: public; Owner: root
+--
+
+INSERT INTO public.order_item (order_id, product_id, quantity) VALUES ('00243f9a-a636-4ba5-91de-be95a91c7ba7', '10a6fcf5-7cc3-4b92-b700-0c8c92f5a51c', 4);
+INSERT INTO public.order_item (order_id, product_id, quantity) VALUES ('c5c267d2-c582-47aa-aff6-207d10db63ac', 'a1ab7d32-75bf-4c28-90c7-4c4167e1e6bd', 2);
+INSERT INTO public.order_item (order_id, product_id, quantity) VALUES ('c5c267d2-c582-47aa-aff6-207d10db63ac', 'a1ab7d32-75bf-4c28-90c7-4c4167e1e6bd', 1);
+INSERT INTO public.order_item (order_id, product_id, quantity) VALUES ('c5c267d2-c582-47aa-aff6-207d10db63ac', '81294c42-7686-4ddd-a324-5a8c3893d16c', 4);
+INSERT INTO public.order_item (order_id, product_id, quantity) VALUES ('ee1efb64-ae33-4963-bd3f-e6263dcf41a6', '96ccc85f-b733-4435-95ab-91bd9644e5c7', 4);
+INSERT INTO public.order_item (order_id, product_id, quantity) VALUES ('ee1efb64-ae33-4963-bd3f-e6263dcf41a6', 'bdecbc3e-e120-44b3-856f-446e56d9b9c1', 2);
+INSERT INTO public.order_item (order_id, product_id, quantity) VALUES ('ee1efb64-ae33-4963-bd3f-e6263dcf41a6', '45701373-c9b7-46b6-9478-acb1c32d92e4', 2);
+INSERT INTO public.order_item (order_id, product_id, quantity) VALUES ('ee1efb64-ae33-4963-bd3f-e6263dcf41a6', 'ce9f3f64-92e5-4f14-8e26-c468d5516dff', 2);
+INSERT INTO public.order_item (order_id, product_id, quantity) VALUES ('ee1efb64-ae33-4963-bd3f-e6263dcf41a6', '2b00bc97-a879-4e97-99ff-ac208bb187ae', 2);
+
+
+--
+-- Name: migrations_id_seq; Type: SEQUENCE SET; Schema: public; Owner: root
+--
+
+SELECT pg_catalog.setval('public.migrations_id_seq', 819, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
